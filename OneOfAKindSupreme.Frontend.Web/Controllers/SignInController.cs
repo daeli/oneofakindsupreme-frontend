684dd43ae3cb417d85a51c67c924ea39fb090b4c
daeli <daelidaily@gmail.com>
2024-04-29 15:59:54 -0500

    Sign in and sign up views.

---
+++ OneOfAKindSupreme.Frontend.Web/Controllers/SignInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OneOfAKindSupreme.Frontend.Web.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OneOfAKindSupreme.Frontend.Web/Controllers/SignUpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OneOfAKindSupreme.Frontend.Web.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
